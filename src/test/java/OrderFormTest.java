2aa6be9a9c13bb13af4ae7624a7a98af8cddb492
Yumetsuki11 <shar2107@yandex.ru>
2022-09-14 22:07:38 +0300

    WebDriverManager added

--- src/test/java/OrderFormTest.java
+++ src/test/java/OrderFormTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,7 +10,7 @@ public class OrderFormTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", ".\\drivers\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
